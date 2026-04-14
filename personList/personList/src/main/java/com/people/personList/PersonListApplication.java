32d6ffa54e38de547addd15ebf1a1d8d99c8ca56
system sylvere BARINDA <systembarinda@gmail.com>
2026-04-14 22:07:26 +0200

    done

---
+++ personList/personList/src/main/java/com/people/personList/PersonListApplication.java
@@ -0,0 +1,13 @@
+package com.people.personList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonListApplication.class, args);
+	}
+
+}
---
+++ personList/personList/src/test/java/com/people/personList/PersonListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.people.personList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
